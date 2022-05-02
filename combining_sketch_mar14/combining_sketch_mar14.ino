23c3c920eac39cf594cfb9776f766bab8c22b8c4
Sid Parikh <parikhsid16@gmail.com>
2022-05-02 00:47:06 -0400

    testing fixes

--- combining_sketch_mar14/combining_sketch_mar14.ino
+++ combining_sketch_mar14/combining_sketch_mar14.ino
@@ -1,8 +1,8 @@
 #include <EEPROM.h>
 
 // Pin constants
-constexpr byte moisture_sensor_pin{0};
-constexpr byte moisture_power_pin{0};
+constexpr byte moisture_sensor_pin{21};
+constexpr byte moisture_power_pin{19};
 constexpr byte rain_valve_pin{0};
 constexpr byte tap_valve_pin{0};
 constexpr byte flow_sensor_pin{0};
@@ -14,13 +14,17 @@ constexpr int eeprom_size{1};
 
 // Conversion constants
 constexpr double moisture_diff_to_flow_ratio{0.5};
-constexpr int microseconds_to_seconds{1000000};
+constexpr long sleep_duration_microseconds{0.01 * 60 * 1000000};
 
 // Watering constants
 constexpr double minimum_water_flow_rate{0}; // Should probably be 0 or a very small number
 
+// byte minimum_moisture)leve
+
 // Water flow variables
-volatile int flow_frequency{0};
+volatile int flow_frequency {
+  0
+};
 double total_flow{0};
 double target_flow{0};
 unsigned long cloop_time{0};
@@ -45,6 +49,11 @@ State system_status{State::sensing};
 // Track the watering status
 WateringState watering_status{WateringState::none};
 
+// This function is the interrupt called on rising edges
+void flow() {
+  flow_frequency++;
+}
+
 void start_flow_meter() {
   digitalWrite(flow_power_pin, HIGH);
 
@@ -62,7 +71,7 @@ bool check_target_met_and_close(byte valve_pin) {
     detachInterrupt(digitalPinToInterrupt(flow_sensor_pin));
     flow_frequency = 0;
     total_flow = 0;
-    
+
     return true;
   }
   return false;
@@ -71,15 +80,14 @@ bool check_target_met_and_close(byte valve_pin) {
 // Returns the current flow rate in Liters per minute
 // Assumes
 double get_flow_rate(unsigned long current_time) {
-  // Save current register state (includes interrupts flag)
-  uint8_t oldSREG = SREG;
+
   // disable interrupts while we read flow_frequency or we might get an
   // inconsistent value (e.g. in the middle of a write to flow_frequency)
   noInterrupts();
   double latestFlow {flow_frequency};
   flow_frequency = 0;
-  // return register to old state
-  SREG = oldSREG;
+  // Return to interrupting
+  interrupts();
 
   // Latest flow is in Hz
   latestFlow /= ((current_time - cloop_time) / 1000.0);
@@ -90,27 +98,37 @@ double get_flow_rate(unsigned long current_time) {
 
 // Called every loop when the state is "watering"
 State water() {
-  switch(watering_status) {
-    case WateringState::none: 
+  Serial.print("\t WS: ");
+  Serial.println(static_cast<int>(watering_status));
+
+
+  unsigned long current_time{0};
+  switch (watering_status) {
+    case WateringState::none:
       // Watering just started
       // Open rainwater valve
       digitalWrite(rain_valve_pin, HIGH);
-      
+
       start_flow_meter();
 
       watering_status = WateringState::rain;
-      
+
       return State::watering;
       break;
     case WateringState::rain:
       // Rainwater valve is open
       // Only act every second
-      unsigned long current_time = millis();
-      if (currentTime >= cloop_time + 1000) {
+      current_time = millis();
+      if (current_time >= cloop_time + 1000) {
         // It's been a second, act on the flow meter and then mark the new time
-       
+
+        double latest_flow{get_flow_rate(current_time)};
+
         // Convert L/min to L/sec and then add to total flow by mutiplying flow rate in L/sec by the time in seconds
-        total_flow += (get_flow_rate(current_time) / 60.0) * ((current_time - cloop_time) / 1000.0));
+        total_flow += ((latest_flow / 60.0) * ((current_time - cloop_time) / 1000.0));
+
+        Serial.print("\t Tot. Flow");
+        Serial.println(total_flow);
 
         if (check_target_met_and_close(rain_valve_pin)) {
           // Target has been met; enter sleep state
@@ -119,7 +137,7 @@ State water() {
         }
 
         // If rainwater isn't working, switch to tap water
-        if (!(one_minute > minimum_water_flow_rate)) {
+        if (!(latest_flow > minimum_water_flow_rate)) {
           // Close rainwater valve
           digitalWrite(rain_valve_pin, LOW);
           // Open tap water valve
@@ -128,18 +146,19 @@ State water() {
         }
 
         // Mark new time
-        cloop_time = currentTime;
+        cloop_time = current_time;
 
         return State::watering;
       }
+      return State::watering;
       break;
     case WateringState::tap:
       // Tap water valve is open
       // Only act every second
-      unsigned long current_time = millis();
-      if (currentTime >= cloop_time + 1000) {
+      current_time = millis();
+      if (current_time >= cloop_time + 1000) {
         // Convert L/min to L/sec and then add to total flow by mutiplying flow rate in L/sec by the time in seconds
-        total_flow += (get_flow_rate(current_time) / 60.0) * ((current_time - cloop_time) / 1000.0));
+        total_flow += ((get_flow_rate(current_time) / 60.0) * ((current_time - cloop_time) / 1000.0));
 
         if (check_target_met_and_close(rain_valve_pin)) {
           // Target has been met; enter sleep state
@@ -154,7 +173,7 @@ State water() {
         }
 
         // Mark new time
-        cloop_time = currentTime;
+        cloop_time = current_time;
 
         return State::watering;
       }
@@ -172,6 +191,10 @@ State sense() {
   delay(10);
   int moisture_level = analogRead(moisture_sensor_pin);
   digitalWrite(moisture_power_pin, LOW);
+
+  Serial.print("\t ML: ");
+  Serial.println(moisture_level);
+  delay(2000);
 
   // Read ESP32 Flash memory for minimum moisture level
   byte minimum_moisture_level = EEPROM.read(eeprom_address_moisture_level);
@@ -195,47 +218,50 @@ State sense() {
 // Called every loop when the state is "sleeping"
 // Should only run once because, well, the board will sleep
 State sleep() {
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  esp_sleep_enable_timer_wakeup(sleep_duration_microseconds);
 
   esp_deep_sleep_start();
 
-  return State::sleeping;  
+  return State::sensing;
 }
 
-// This function is the interrupt called on rising edges
-void flow() {
-  flow_frequency++;
-}
+
 
 // Setup the pins on startup
 void initGPIO() {
   pinMode(moisture_sensor_pin, INPUT);
   pinMode(moisture_power_pin, OUTPUT);
-  
+
   pinMode(rain_valve_pin, OUTPUT);
   pinMode(tap_valve_pin, OUTPUT);
 
   pinMode(flow_sensor_pin, INPUT);
-  pinMode(flow_power_pin, OUTPUT); // NEEDS TESTING 
+  pinMode(flow_power_pin, OUTPUT); // NEEDS TESTING
   // Pull up flow pin
   digitalWrite(flow_sensor_pin, HIGH);
-  
+
 }
 
 // On Power Cycle
 void setup() {
   initGPIO();
-  
+
   // Initialize EEPROM
   EEPROM.begin(eeprom_size);
+  // EEPROM.write(eeprom_address_moisture_level, 100);
 
   /* Check for BT connection and write preferences as needed here ..... */
 
   // Make no assumptions and go back into sensing mode.
   system_status = State::sensing;
+
+  Serial.begin(112500);
 }
 
 void loop() {
+  Serial.print("Status: ");
+  Serial.println(static_cast<int>(system_status));
+
   switch (system_status) {
     case State::sensing: system_status = sense(); break;
     case State::watering: system_status = water(); break;
